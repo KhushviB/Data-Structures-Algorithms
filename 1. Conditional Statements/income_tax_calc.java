e243cc9d842a6af288f8334b2e022463c71ef5d3
Khushvi Bamrolia <khushvi.bamrolia28@gmail.com>
2024-08-27 14:02:07 +0530

    Initial commit

---
+++ 1. Conditional Statements/income_tax_calc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class income_tax_calc {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int income = sc.nextInt();
+        if(income < 500000){
+            System.out.println("YOur tax is : 0");
+        } else if(income >= 500000 && income <= 1000000){
+            System.out.println("Your tax is : " + income * 0.2);
+        } else {
+            System.out.println("Your tax is : " + income * 0.3);
+        }
+    }
+}
