8e64d8ab764e320afd096a995742657976e8887f
Khushvi Bamrolia <khushvi.bamrolia28@gmail.com>
2024-08-29 20:20:47 +0530

    Initial commit

---
+++ 3. Functions & methods/product_ab.java
@@ -0,0 +1,12 @@
+public class product_ab {
+    public static void main(String args[]){
+        int a = 4;
+        int b = 7;
+        int ans = multiply(a,b);
+        System.out.println("The product is : " + ans);
+    }
+    public static int multiply(int a, int b){
+        int product = a * b;
+        return product;
+    }
+}
