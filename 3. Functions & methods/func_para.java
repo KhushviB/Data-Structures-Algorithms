f326e1e6bc0933e6b73d7bc94d3279a3c15bddfc
Khushvi Bamrolia <khushvi.bamrolia28@gmail.com>
2024-08-29 20:08:40 +0530

    This is the syntax for function with parameters

---
+++ 3. Functions & methods/func_para.java
@@ -0,0 +1,11 @@
+public class func_para {
+    public static void main(String args[]){
+        para(2,66);
+    }
+
+    public static void para(int x, int y){
+        int sum = x+y;
+        System.out.println("The sum of " + x + " & " + y + " is : " + sum);
+    }
+    
+}
