bf2ca7986c3d73ce88a3285c69cbaa553c2ba6b6
Khushvi Bamrolia <khushvi.bamrolia28@gmail.com>
2024-08-29 19:46:29 +0530

    This is the basic syntax of function & methods

---
+++ 3. Functions & methods/func_basics.java
@@ -0,0 +1,8 @@
+public class func_basics {
+    public static void main(String args[]){
+        message();
+    }
+    public static void message(){
+        System.out.println("This is the function call of message");
+    }
+}
