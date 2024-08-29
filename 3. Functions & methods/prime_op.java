795fd24a2764192305d4de8e0bc1f96c7769946a
Khushvi Bamrolia <khushvi.bamrolia28@gmail.com>
2024-08-30 01:03:04 +0530

    This is the optimized code to check prime number

---
+++ 3. Functions & methods/prime_op.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class prime_op {
+    public static void main(String args[]){
+        System.out.println(check_prime(25));
+    }
+    public static boolean check_prime(int n){
+        if(n==2){
+            return true;
+        }
+
+        for(int i =2; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
